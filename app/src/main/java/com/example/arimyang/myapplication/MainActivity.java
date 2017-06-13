2992fc6c8fb54c489cdcf768c6732c3a24b91814
yangarim <dlsdjdkfla@naver.com>
2017-06-13 11:11:09 +0900

    second

--- app/src/main/java/com/example/arimyang/myapplication/MainActivity.java
+++ app/src/main/java/com/example/arimyang/myapplication/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.arimyang.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import net.daum.mf.map.api.MapView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapView mapView = new MapView(MainActivity.this);
+        mapView.setDaumMapApiKey("13ac7cd3b1977e88517505c5617e2a68");
+
+        RelativeLayout mapViewContainer = (RelativeLayout) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
     }
 }
