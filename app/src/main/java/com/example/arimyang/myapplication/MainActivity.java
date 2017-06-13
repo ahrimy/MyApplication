27c494a6748cf3d2dd7c322aaa0acd801698da17
yangarim <dlsdjdkfla@naver.com>
2017-06-14 03:58:03 +0900

    New

--- app/src/main/java/com/example/arimyang/myapplication/MainActivity.java
+++ app/src/main/java/com/example/arimyang/myapplication/MainActivity.java
@@ -1,24 +1,48 @@
 package com.example.arimyang.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
-import net.daum.mf.map.api.MapView;
+import com.skp.Tmap.TMapMarkerItem;
+import com.skp.Tmap.TMapPoint;
+import com.skp.Tmap.TMapView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private final String TMAP_API_KEY = "cbe6c66f-2b57-3ee7-a174-ed80856248ae";
+    private TMapView tmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MapView mapView = new MapView(MainActivity.this);
-        mapView.setDaumMapApiKey("13ac7cd3b1977e88517505c5617e2a68");
+        initTmap();
+    }
 
-        RelativeLayout mapViewContainer = (RelativeLayout) findViewById(R.id.map_view);
-        mapViewContainer.addView(mapView);
+    private void initTmap() {
+        tmap = new TMapView(this);
+        tmap.setSKPMapApiKey(TMAP_API_KEY);
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.main_layout_tmap);
 
+        double lat = 37.5025595;
+        double lng = 126.8423617;
+        tmap.setCenterPoint(lng, lat);
+        tmap.setZoomLevel(15);
+
+        layout.addView(tmap);
+
+        addMarker();
+    }
+    private void addMarker() {
+        double lat = 37.5025595;
+        double lng = 126.8423617;
+
+        TMapPoint point = new TMapPoint(lat,lng);
+        TMapMarkerItem marker = new TMapMarkerItem();
+        marker.setTMapPoint(point);
+        tmap.addMarkerItem("marker",marker);
     }
 }
---
+++ app/src/main/java/com/example/arimyang/myapplication/MapPoint.java
@@ -0,0 +1,39 @@
+package com.example.arimyang.myapplication;
+
+/**
+ * Created by ArimYang on 2017-06-14.
+ */
+
+public class MapPoint {
+    private String Name;
+    private double latitude;
+    private double longitude;
+
+    public MapPoint(){
+        super();
+    }
+    public MapPoint(String Name,double latitude,double longitude){
+        this.Name = Name;
+        this.latitude =latitude;
+        this.longitude = longitude;
+    }
+
+    public String getName(){
+        return Name;
+    }
+    public void setName(String Name){
+        this.Name = Name;
+    }
+    public double getLatitude(){
+        return latitude;
+    }
+    public void setLatitude(double latitude){
+        this.latitude = latitude;
+    }
+    public double getLongitude(){
+        return longitude;
+    }
+    public void setLongitude(double longitude){
+        this.longitude = longitude;
+    }
+}
