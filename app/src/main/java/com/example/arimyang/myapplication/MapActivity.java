7fafbd3c89a46b6fa3e9dce58fbce70e24a1a20f
yangarim <dlsdjdkfla@naver.com>
2017-06-26 14:54:04 +0900

    temp

---
+++ app/src/main/java/com/example/arimyang/myapplication/MapActivity.java
@@ -0,0 +1,48 @@
+package com.example.arimyang.myapplication;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import com.skp.Tmap.TMapMarkerItem;
+import com.skp.Tmap.TMapPoint;
+import com.skp.Tmap.TMapView;
+
+public class MapActivity extends Activity {
+
+    private final String TMAP_API_KEY = "cbe6c66f-2b57-3ee7-a174-ed80856248ae";
+    private TMapView tmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initTmap();
+    }
+
+    private void initTmap() {
+        tmap = new TMapView(this);
+        tmap.setSKPMapApiKey(TMAP_API_KEY);
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.main_layout_tmap);
+
+        double lat = 37.5025595;
+        double lng = 126.8423617;
+        tmap.setCenterPoint(lng, lat);
+        tmap.setZoomLevel(15);
+
+        layout.addView(tmap);
+
+        addMarker();
+    }
+    private void addMarker() {
+        double lat = 37.5025595;
+        double lng = 126.8423617;
+
+        TMapPoint point = new TMapPoint(lat,lng);
+        TMapMarkerItem marker = new TMapMarkerItem();
+        marker.setTMapPoint(point);
+        tmap.addMarkerItem("marker",marker);
+    }
+}
--- app/src/main/java/com/example/arimyang/myapplication/Path.java
+++ app/src/main/java/com/example/arimyang/myapplication/Path.java
@@ -1,8 +1,14 @@
 package com.example.arimyang.myapplication;
 
+import com.skp.Tmap.TMapData;
+
 /**
  * Created by ArimYang on 2017-06-26.
  */
 
 public class Path {
+    TMapData data;
+    public Path(TMapData tmapdata){
+        data = tmapdata;
+    }
 }
